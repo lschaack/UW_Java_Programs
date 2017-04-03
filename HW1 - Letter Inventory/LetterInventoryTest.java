1151301f05310a8b3fb164be2fbcc949ea42db07
lschaack <luke.schaack@gmail.com>
2017-04-03 13:52:30 -0600

    deleting everything

--- HW1 - Letter Inventory/LetterInventoryTest.java
+++
@@ -1,8 +0,0 @@
-public class LetterInventoryTest {
-   public static void main(String[] args) {
-      LetterInventory test = new LetterInventory("Hillary Rodham Clinton");
-      System.out.println(test.size());
-      System.out.println(test);
-      System.out.println(test.getLetterPercentage('-'));
-   }
-}
